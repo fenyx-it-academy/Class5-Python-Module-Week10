86f91794ed925f50da1babc5a25baf5950843cd3
hltaydn <91338529+hltaydn@users.noreply.github.com>
2021-11-25 18:56:35 +0100

    Update exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -8,7 +8,7 @@ def connect():
             host="localhost",
             database="PyCoders",
             user="postgres",
-            password="Halo107.")
+            password="password")
 
         # create a cursor
         cur = conn.cursor()
